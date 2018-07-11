8c0be142a7cb47b98d24aa53a29b7d83cd4d4644
191511040 <40803416+191511040@users.noreply.github.com>
2018-07-11 14:23:37 +0530

    Update hello count .py

--- codekatta/hello count .py
+++ codekatta/hello count .py
@@ -1,10 +1,3 @@
-a=raw_input()
-if a.isalpha():
- print("invalid")
-else:
-  a=int(a)
-  count=0
-    while a!=0:
-      a=int(a/10)
-      count=count+1
-   print(count)
+a=int(raw_input())
+for number in range(a):
+   print("hello")
