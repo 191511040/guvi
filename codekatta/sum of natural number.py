9e2a4f6475491d997b0abe709f8018fbe9e9e979
191511040 <40803416+191511040@users.noreply.github.com>
2018-07-12 10:56:46 +0530

    Update sum of natural number.py

--- codekatta/sum of natural number.py
+++ codekatta/sum of natural number.py
@@ -1,9 +1,9 @@
 if a < 0:
-   print("Enter a positive number")
+   print("Enter a number")
 else:
    sum = 0
    
    while(a > 0):
        sum += num
        num -= 1
-   print("The sum is",sum)
+   print("The output is",sum)
