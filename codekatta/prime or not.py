223616b71834d869f941fa36349c57ebca9e8e83
191511040 <40803416+191511040@users.noreply.github.com>
2018-07-18 13:07:55 +0530

    Update prime or not.py

--- codekatta/prime or not.py
+++ codekatta/prime or not.py
@@ -4,10 +4,8 @@ if a > 1:
    for i in range(2,a):  
        if (a % i) == 0:  
            print("no")  
-           
            break  
-   else:  
+else:  
        print("yes")  
-         
 else:  
    print("no")  
