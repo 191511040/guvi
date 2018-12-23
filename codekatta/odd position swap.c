34d2c465de030e3ac614339aaa9cba8ec8482782
191511040 <40803416+191511040@users.noreply.github.com>
2018-12-24 00:17:32 +0530

    Update odd position swap.c

--- codekatta/odd position swap.c
+++ codekatta/odd position swap.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
-#include<string.h>
-int main()
-{   
-    char str[20];
-    int b,i,j,temp;
-    printf("enter a string");
-    scanf("%s",str);
-    b=strlen(str);
-    while(b<=20)
-    {
-        for(i=0;i<=strlen(str);i=i+2)
-        {
-            
-            
-                temp=str[i];
-                str[i]=str[i+1];
-                str[i+1]=temp;
-                
-          
-            
-            
-        }
-        printf("%s",str);
-    }
+#include <string.h>
+void main(){
+     char str[20],tmp;
+     int i,j;
+     scanf("%s",str);
+     printf("\n\nOriginal String     : %s",str);
+     for(i=0;i<strlen(str);i=i+2){
+     tmp = str[i];
+     str[i] = str[i+1];
+     str[i+1] = tmp;
+     }
+     printf(" %s",str);
+     
 }
-    
