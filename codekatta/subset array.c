8fc5195de6b62d58d95259de81efd8e49b0f8cbc
191511040 <40803416+191511040@users.noreply.github.com>
2019-01-06 13:53:46 +0530

    Update subset array.c

--- codekatta/subset array.c
+++ codekatta/subset array.c
@@ -4,12 +4,12 @@ int main()
 {
   int a,b,i,c[20],d[20],j;
   scanf("%d%d",&a,&b);
-  printf("enter the a elements");
+  
   for(i=0;i<a;i++)
   {
       scanf("%d",&c[i]);
   }
-  printf("enter the b elements");
+ 
   for(j=0;j<b;j++)
   {
       scanf("%d",&d[j]);
