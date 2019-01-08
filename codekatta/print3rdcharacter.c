43f0d45b63d0280230d040573ddd2d7471b5725a
191511040 <40803416+191511040@users.noreply.github.com>
2019-01-08 19:40:43 +0530

    Create print3rdcharacter.c

---
+++ codekatta/print3rdcharacter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{   
+    int len,i=0;
+    char a[30];
+    scanf("%s",&a);
+    for(i=0;a[i]!=0;i=i+3)
+    {
+        printf("%c",a[i]);
+    }
